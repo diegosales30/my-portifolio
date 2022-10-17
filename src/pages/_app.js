4b03914dd9748da74bea32f28af910c62f1d7eb9
diego <diegofelipesales23@gmail.com>
2022-10-16 21:35:11 -0300

    adicionando acessibilidade com vlibras no nextjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import "../../styles/globals.scss";
 import Provider from "../Providers";
 import Header from "../components/Header";
+import VLibras from "vlibras-nextjs";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
       <Header />
+      <div>
+        {/* only worked in production in tests with nextjs applications maybe you can solve this! */}
+        {process.env.NODE_ENV === "production" && <VLibras forceOnload />}
+      </div>
       <Component {...pageProps} />
     </Provider>
   );
