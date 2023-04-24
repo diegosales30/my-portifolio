b58e179fd1801a67c208a4b3c5cf2befe6b3b0b2
diegosales <diegofelipesales23@gmail.com>
2023-04-24 01:43:41 -0400

    add novo projeto, twtter clone

--- src/components/Swiper/index.jsx
+++ src/components/Swiper/index.jsx
@@ -8,6 +8,7 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 
+import img0 from "../../assets/images/twitter-clone.png"
 import img1 from "../../assets/images/do-it.png";
 import img2 from "../../assets/images/easymovie.png";
 import img4 from "../../assets/images/nukenzie.png";
@@ -17,6 +18,9 @@ import img6 from "../../assets/images/tic.png";
 import styles from "./Swipers.module.scss";
 
 const dados = [
+  {
+    img: img0,
+  },
   {
     img: img1,
   },
