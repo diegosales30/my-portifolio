3d5a7a14a2d2d3a0cd052a2e6bbc25dc1fc51a7a
Diego sales <diegofelipesales23@gmail.com>
2023-05-15 03:05:38 -0400

    add novo projeto

--- src/components/Swiper/index.jsx
+++ src/components/Swiper/index.jsx
@@ -8,10 +8,10 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 
-
-import img0 from "../../assets/images/doitnow.png";
-import img1 from "../../assets/images/twitter-clone.png"
-import img2 from "../../assets/images/easymovie.png";
+import img0 from "../../assets/images/ia-resumidor.png"
+import img1 from "../../assets/images/doitnow.png";
+import img2 from "../../assets/images/twitter-clone.png"
+import img3 from "../../assets/images/easymovie.png";
 import img4 from "../../assets/images/nukenzie.png";
 import img5 from "../../assets/images/organizze.png";
 import img6 from "../../assets/images/tic.png";
@@ -28,6 +28,9 @@ const dados = [
   },
   {
     img: img2,
+  },
+  {
+    img: img3,
   },
   {
     img: img4,
